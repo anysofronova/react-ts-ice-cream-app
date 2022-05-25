0f6c2f8b05b5ed22de68848d177f86f8b34fe938
Anna <anysofronova@yandex.ru>
2022-05-25 13:38:40 +0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
