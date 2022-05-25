d9fbf808307563b725428c1c73959b27986f3850
Anna <anysofronova@yandex.ru>
2022-05-25 15:15:05 +0300

    Sort component

---
+++ src/components/Filters/Sort/Sort.tsx
@@ -0,0 +1,26 @@
+import NativeSelect from "@mui/material/NativeSelect";
+import styles from "./Sort.module.scss";
+import { FormControl, InputLabel } from "@mui/material";
+
+const Sort = () => {
+  return (
+    <FormControl className={styles.sort}>
+      <InputLabel variant="standard" htmlFor="uncontrolled-native">
+        Sort by:
+      </InputLabel>
+      <NativeSelect
+        defaultValue={"best"}
+        inputProps={{
+          title: "title",
+          id: "number",
+        }}
+      >
+        <option value={"best"}>Best-selling</option>
+        <option value={"up"}>Price: Low to High</option>
+        <option value={"down"}>Price: High to Low</option>
+      </NativeSelect>
+    </FormControl>
+  );
+};
+
+export default Sort;
