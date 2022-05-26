53a3bb9833e7ff46e7d610c22ee1ae54948360a9
Anna <anysofronova@yandex.ru>
2022-05-26 09:34:11 +0300

    Counter of ice-Cream

--- src/components/Products/Item/Item.tsx
+++ src/components/Products/Item/Item.tsx
@@ -1,8 +1,10 @@
 import styles from "./Item.module.scss";
 import photo from "../../../assets/iceCream/1.jpg";
 import { AiOutlinePlus } from "react-icons/ai";
+import { useState } from "react";
 
 const Item = ({ title, price }: { title: string; price: number }) => {
+  const [iceCreamCount, setIceCreamCount] = useState(0);
   return (
     <div className={styles.item}>
       <img src={photo} alt="ice-Cream" className={styles.img} />
@@ -10,18 +12,19 @@ const Item = ({ title, price }: { title: string; price: number }) => {
       <div className={styles.filter}>
         <ul className={styles.container}>
           <li>Waffle cone</li>
-          <li>Eco - container</li>
+          <li className={styles.active}>Eco - container</li>
         </ul>
         <ul className={styles.count}>
-          <li>1b.</li>
+          <li className={styles.active}>1b.</li>
           <li>2b.</li>
           <li>3b.</li>
         </ul>
       </div>
       <div className={styles.buying}>
         <div className={styles.price}>From {price}$</div>
-        <button>
+        <button onClick={() => setIceCreamCount(iceCreamCount + 1)}>
           <AiOutlinePlus /> Add
+          <span>{iceCreamCount}</span>
         </button>
       </div>
     </div>
