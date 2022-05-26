7e666199e9df5e61d26f3849cd915614f0a6a087
Anna <anysofronova@yandex.ru>
2022-05-26 13:56:00 +0300

    Sum ice Cream prices and download the data from server

--- src/components/Products/Item/Item.tsx
+++ src/components/Products/Item/Item.tsx
@@ -2,17 +2,22 @@ import styles from "./Item.module.scss";
 import { AiOutlinePlus } from "react-icons/ai";
 import { useState } from "react";
 import placeholder from "../../../assets/imgPlaceholder.jpeg";
+import cn from "classnames";
 
 const Item = ({
   title,
   price,
   photo,
 }: {
-  title: string;
-  price: number;
+  title: string | null;
+  price: number[];
   photo: string | undefined;
 }) => {
   const [iceCreamCount, setIceCreamCount] = useState(0);
+  const [container, setContainer] = useState(0);
+  const [ballsCount, setBallsCount] = useState(0);
+  const sizes: string[] = ["1b.", "2b.", "3b."];
+  const containerPrice = [0.47, 0.22];
   return (
     <div className={styles.item}>
       <div>
@@ -26,17 +31,35 @@ const Item = ({
       <div>
         <div className={styles.filter}>
           <ul className={styles.container}>
-            <li>Waffle cone</li>
-            <li className={styles.active}>Eco - container</li>
+            <li
+              className={cn(container || styles.active)}
+              onClick={() => setContainer(0)}
+            >
+              Waffle cone
+            </li>
+            <li
+              className={cn(container && styles.active)}
+              onClick={() => setContainer(1)}
+            >
+              Eco - container
+            </li>
           </ul>
           <ul className={styles.count}>
-            <li className={styles.active}>1b.</li>
-            <li>2b.</li>
-            <li>3b.</li>
+            {sizes.map((val, idx) => (
+              <li
+                key={val}
+                className={cn(ballsCount === idx && styles.active)}
+                onClick={() => setBallsCount(idx)}
+              >
+                {val}
+              </li>
+            ))}
           </ul>
         </div>
         <div className={styles.buying}>
-          <div className={styles.price}>From {price}$</div>
+          <div className={styles.price}>
+            {(price[ballsCount] + containerPrice[container]).toFixed(2)}$
+          </div>
           <button onClick={() => setIceCreamCount(iceCreamCount + 1)}>
             <AiOutlinePlus /> Add
             <span>{iceCreamCount}</span>
--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,13 +1,20 @@
 import Item from "./Item/Item";
 import styles from "./Products.module.scss";
-import { iceCreams } from "../../data/iceCreams";
+import { useEffect, useState } from "react";
 
 const Products = () => {
+  const [items, setItems] = useState([] as any[]);
+  useEffect(() => {
+    fetch("https://628e254ba339dfef87a86df6.mockapi.io/items?p=1&l=4")
+      .then((r) => r.json())
+      .then((arr) => setItems(arr));
+  }, []);
+
   return (
     <div className={styles.products}>
-      {iceCreams.map((i) => (
+      {items.map((i) => (
         <Item
-          price={i.prices[0][0] + i.prices[1][1]}
+          price={i.iceCreamPrices}
           title={i.title}
           photo={i.imgUrl}
           key={i.id}
