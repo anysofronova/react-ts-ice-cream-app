1e85dff7d49318e740bf0e470456274996fac4c8
Anna <anysofronova@yandex.ru>
2022-05-26 14:17:28 +0300

    Pagination

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,26 +1,40 @@
 import Item from "./Item/Item";
 import styles from "./Products.module.scss";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { Pagination } from "@mui/material";
 
 const Products = () => {
   const [items, setItems] = useState([] as any[]);
+  const [pageCount, setPageCount] = useState(1);
   useEffect(() => {
-    fetch("https://628e254ba339dfef87a86df6.mockapi.io/items?p=1&l=4")
+    fetch(
+      `https://628e254ba339dfef87a86df6.mockapi.io/items?p=${pageCount}&l=5`
+    )
       .then((r) => r.json())
       .then((arr) => setItems(arr));
-  }, []);
+  }, [pageCount]);
 
   return (
-    <div className={styles.products}>
-      {items.map((i) => (
-        <Item
-          price={i.iceCreamPrices}
-          title={i.title}
-          photo={i.imgUrl}
-          key={i.id}
+    <Fragment>
+      <div className={styles.products}>
+        {items.map((i) => (
+          <Item
+            price={i.iceCreamPrices}
+            title={i.title}
+            photo={i.imgUrl}
+            key={i.id}
+          />
+        ))}
+      </div>
+      <div className={styles.pagination}>
+        <Pagination
+          count={3}
+          onChange={(_, num) => setPageCount(num)}
+          variant="outlined"
+          color="primary"
         />
-      ))}
-    </div>
+      </div>
+    </Fragment>
   );
 };
 
