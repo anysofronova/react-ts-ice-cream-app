59310c3f00ebfd7cfa34cacb8569a5900c0cb9cd
Anna <anysofronova@yandex.ru>
2022-05-26 18:03:46 +0300

    Skeleton

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -2,29 +2,36 @@ import Item from "./Item/Item";
 import styles from "./Products.module.scss";
 import { Fragment, useEffect, useState } from "react";
 import { Pagination } from "@mui/material";
+import Skeleton from "../UI/Skeleton";
 
 const Products = () => {
   const [items, setItems] = useState([] as any[]);
   const [pageCount, setPageCount] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     fetch(
-      `https://628e254ba339dfef87a86df6.mockapi.io/items?p=${pageCount}&l=5`
+      `https://628e254ba339dfef87a86df6.mockapi.io/items?p=${pageCount}&l=6`
     )
-      .then((r) => r.json())
+      .then((r) => {
+        setIsLoading(false);
+        return r.json();
+      })
       .then((arr) => setItems(arr));
   }, [pageCount]);
 
   return (
     <Fragment>
       <div className={styles.products}>
-        {items.map((i) => (
-          <Item
-            price={i.iceCreamPrices}
-            title={i.title}
-            photo={i.imgUrl}
-            key={i.id}
-          />
-        ))}
+        {isLoading
+          ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
+          : items.map((i) => (
+              <Item
+                price={i.iceCreamPrices}
+                title={i.title}
+                photo={i.imgUrl}
+                key={i.id}
+              />
+            ))}
       </div>
       <div className={styles.pagination}>
         <Pagination
