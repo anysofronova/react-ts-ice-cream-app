4c30230c66ce0fdfd0bb35d612ea8a55392074a0
Anna <anysofronova@yandex.ru>
2022-05-27 10:13:00 +0300

    Not Found

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+import styles from "./NotFound.module.scss";
+
+function NotFound() {
+  return (
+    <div className={styles.notFound}>
+      <div className={styles.face}>
+        <div className={styles.band}>
+          <div className={styles.red} />
+          <div className={styles.white} />
+          <div className={styles.blue} />
+        </div>
+        <div className={styles.eyes} />
+        <div className={styles.dimples} />
+        <div className={styles.mouth} />
+      </div>
+
+      <h1 className={styles.title}>Oops! Something went wrong!</h1>
+      <NavLink to={"/"}>
+        <div className={styles.btn}>Return to Home</div>
+      </NavLink>
+    </div>
+  );
+}
+
+export default NotFound;
