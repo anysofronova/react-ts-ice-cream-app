2dffebc13a0030bcae0203d4262952abd90b355c
Anna <anysofronova@yandex.ru>
2022-05-27 19:21:09 +0300

    SEARCHING

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,16 @@ import SearchPanel from "./SearchPanel/SearchPanel";
 import styles from "./Header.module.scss";
 import CartBlock from "./CartBlock/CartBlock";
 
-const Header = () => {
+const Header = (
+  props: JSX.IntrinsicAttributes & { searchValue: any; setSearchValue: any }
+) => {
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
         <Logo />
       </div>
       <div className={styles.searchPanel}>
-        <SearchPanel />
+        <SearchPanel {...props} />
       </div>
       <div className={styles.cart}>
         <CartBlock />
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Products from "../../components/Products/Products";
 
-const Home = () => {
-  return <Products />;
+const Home = (props: JSX.IntrinsicAttributes & { searchValue: any }) => {
+  return <Products {...props} />;
 };
 
 export default Home;
