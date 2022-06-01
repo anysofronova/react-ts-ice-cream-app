817437718a7e03d24e3c5b4e89ce4b77b16177dc
Anna <anysofronova@yandex.ru>
2022-06-01 12:40:09 +0300

    Sale component

---
+++ src/models/ISale.ts
@@ -0,0 +1,3 @@
+export interface ISale {
+  image: string;
+}
---
+++ src/pages/Sale/Sale.tsx
@@ -0,0 +1,24 @@
+import styles from "./Sale.module.scss";
+import SaleItem from "../../components/SaleItem/SaleItem";
+import { productsApi } from "../../services/ProductsService";
+import SkeletonSale from "../../components/UI/SkeletonSale";
+import NotFound from "../NotFound/NotFound";
+
+const Sale = () => {
+  const { data, isLoading, error } = productsApi.useFetchSaleQuery("");
+  return (
+    <div className={styles.sale}>
+      {isLoading ? (
+        [...new Array(4)].map((_, i) => <SkeletonSale key={i} />)
+      ) : error ? (
+        <div className={styles.error}>
+          <NotFound />
+        </div>
+      ) : (
+        data && data.map((i, idx) => <SaleItem image={i.image} key={idx} />)
+      )}
+    </div>
+  );
+};
+
+export default Sale;
