cf3abd2e0596a7c8faab0c8a3629d510cf613537
Anna <anysofronova@yandex.ru>
2022-06-01 12:29:07 +0300

    Navigation in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,20 +2,24 @@ import Logo from "./Logo/Logo";
 import SearchPanel from "./SearchPanel/SearchPanel";
 import styles from "./Header.module.scss";
 import CartBlock from "./CartBlock/CartBlock";
+import Navigation from "./Navigation/Navigation";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <div className={styles.logo}>
-        <Logo />
+    <>
+      <div className={styles.header}>
+        <div className={styles.logo}>
+          <Logo />
+        </div>
+        <div className={styles.searchPanel}>
+          <SearchPanel />
+        </div>
+        <div className={styles.cart}>
+          <CartBlock />
+        </div>
       </div>
-      <div className={styles.searchPanel}>
-        <SearchPanel />
-      </div>
-      <div className={styles.cart}>
-        <CartBlock />
-      </div>
-    </div>
+      <Navigation />
+    </>
   );
 };
 
