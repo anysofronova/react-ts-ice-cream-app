c44f11d9bcbd9884442f3aef312f605b036f6dfe
Anna <anysofronova@yandex.ru>
2022-06-01 12:40:20 +0300

    About component

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,42 @@
+import styles from "./About.module.scss";
+const About = () => {
+  return (
+    <div className={styles.about}>
+      <h2>Our Story</h2>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aspernatur
+        dolorem est, nam quia reiciendis velit? A architecto commodi
+        consequuntur, cumque cupiditate delectus dicta enim, error incidunt
+        inventore iusto laudantium magnam maiores odit porro possimus provident
+        quos ratione reprehenderit repudiandae sapiente sequi sit, sunt ullam
+        veniam veritatis voluptatibus. A alias animi autem commodi consectetur
+        consequatur consequuntur corporis ex excepturi fuga inventore, ipsam
+        ipsum laborum maxime molestiae molestias mollitia nemo neque nihil nisi
+        non omnis pariatur quaerat quasi quisquam repellat similique sit soluta
+        tempore voluptas.
+      </div>
+
+      <div>
+        Asperiores assumenda commodi culpa cum dolores, ea iste molestias nam
+        recusandae! A accusantium aliquid consectetur corporis debitis dolor
+        dolorum error fuga id illo ipsa iste iure laboriosam libero minus modi
+        nemo nesciunt officiis placeat quae quia, quo reiciendis repellendus
+        sequi similique sit ullam unde vel vero voluptatem? Ab dolorum hic
+        inventore minima obcaecati odio quibusdam soluta. Dolor doloribus, harum
+        natus nostrum odio quasi quod.
+      </div>
+      <div>
+        Accusamus ad amet architecto cum cumque deserunt dolore dolorem earum et
+        eveniet explicabo illo ipsam iusto labore molestias non nostrum pariatur
+        quam quas quia quidem, quisquam quo quos rerum saepe sit totam! Alias
+        asperiores aspernatur beatae culpa cumque cupiditate debitis dicta
+        dolore dolorem doloremque ea error, eum expedita explicabo hic illo
+        impedit inventore ipsam magni modi necessitatibus nesciunt nihil odio
+        placeat quam quisquam ratione repellat sequi, soluta totam, veniam
+        veritatis voluptate voluptatibus!
+      </div>
+    </div>
+  );
+};
+
+export default About;
